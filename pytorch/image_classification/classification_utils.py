f6b4f46beec4ad3dbe831f003c33ef3fc1e20542
ldickmanns <ludwig.dickmanns@outlook.de>
2021-11-18 10:10:16 +0100

    Added a function to run a single validation epoch to classification_utils.py.

--- pytorch/image_classification/classification_utils.py
+++ pytorch/image_classification/classification_utils.py
@@ -31,3 +31,26 @@ def train_epoch(
 
     epoch_loss /= len(train_loader.dataset)
     return epoch_loss
+
+
+def valid_epoch(
+    model: Module,
+    valid_loader: DataLoader,
+    criterion: Loss,
+    device: str
+) -> float:
+    model.eval().to(device)
+    epoch_loss = 0.0
+
+    for X, y in valid_loader:
+        X = X.to(device)
+        y = y.to(device)
+
+        # Forward
+        with torch.no_grad():
+            outputs = model(X)
+            loss = criterion(outputs, y)
+            epoch_loss += loss.item() * X.size(0)
+
+    epoch_loss /= len(valid_loader.dataset)
+    return epoch_loss
